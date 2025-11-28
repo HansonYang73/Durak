2e07f314d826038e504ccd835b17013b48e8e709
Hanson Yang <hansonyang73@gmail.com>
2025-11-27 21:42:29 -0500

    Finished the basics of Card

---
+++ Durak/Durak/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durak
+{
+    internal class Card
+    {
+        private int power;
+        public int Power { 
+            get
+            {
+                return power;
+            } 
+            set
+            {
+                power = value % 13;
+                switch (power)
+                {
+                    case 0:
+                        power = 13;
+                        break;
+                    case 1:
+                        power = 14;
+                        break;
+                }
+
+                if (Suit == EmpSuit)
+                {
+                    power += 13;
+                }
+            } 
+        }
+        public Suit Suit { get; set; }
+        public static Suit EmpSuit { get; set; } // Empowered Suit
+        public string CardImg {  get; set; }
+
+        public Card(int num, Suit suit)
+        {
+            Suit = suit;
+            Power = num;
+            CardImg = num + ".png";
+        }
+    }
+}
---
+++ Durak/Durak/Suit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durak
+{
+    internal enum Suit
+    {
+        Heart,
+        Diamond,
+        Club,
+        Spade
+    }
+}
