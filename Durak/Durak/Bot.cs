529977f60335018fb1fe888c9af17bfbf6df775a
Hanson Yang <hansonyang73@gmail.com>
2025-12-07 22:01:13 -0500

    small changes to deck and added bot class

---
+++ Durak/Durak/Bot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durak
+{
+    internal class Bot // Add sorting to Deck.cs
+    {
+        private Deck MainDeck { get; set; }
+        private Deck BoardDeck { get; set; }
+        public Bot(Deck deck, Deck boardDeck)
+        {
+            MainDeck = deck;
+            BoardDeck = boardDeck;
+        }
+
+        public Card Attack()
+        {
+            if (CanAttack())
+            {
+
+            }
+        }
+
+        private bool CanAttack()
+        {
+
+        }
+    }
+}
